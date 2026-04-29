f8b7b1d3ab10ad10dfb59afdc661a8df165ba534
Chaitanya <chaitanya@logisticinfotech.co.in>
2026-04-29 12:19:47 +0530

    Complete the demo

--- 03_layout_Design/CustomFontsStyle/App.tsx
+++ 03_layout_Design/CustomFontsStyle/App.tsx
@@ -5,41 +5,53 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
   return (
     <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={styles.container}>
+          <Text style={styles.WeddingdayPersonalUse}>
+            This is the Weddingday Personal Use font
+          </Text>
+          <Text style={styles.DroplineRegularWpegz}>
+            This is the DroplineRegular-Wpegz font
+          </Text>
+          <Text style={styles.Freedom10eM}>
+            This is the Freedom-10eM font
+          </Text>
+        </View>
+      </SafeAreaView>
     </SafeAreaProvider>
   );
 }
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
+export default App;
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "lavender",
     flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 20,
+  },
+  
+  WeddingdayPersonalUse: {
+    fontFamily: "Weddingday Personal Use",
+    fontSize: 50,
+  },
+
+  DroplineRegularWpegz: {
+    fontFamily: "DroplineRegular-Wpegz",
+    fontSize: 50,
+  },
+
+  Freedom10eM: {
+    fontFamily: "Freedom-10eM",
+    fontSize: 28,
   },
 });
-
-export default App;
